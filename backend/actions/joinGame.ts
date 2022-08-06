a9608dbc8913be78a03bb9813d0aa966afcfa2f1
Juha Korhonen <julpero@gmail.com>
2022-08-06 19:57:45 +0300

    test

--- backend/actions/joinGame.ts
+++ backend/actions/joinGame.ts
@@ -1,6 +1,7 @@
 import { IJoinLeaveGameRequest, IJoinLeaveGameResponse, JOIN_LEAVE_RESULT } from "../../frontend/src/interfaces/IGameList";
 
 export const joinGame = async (joinGameRequest: IJoinLeaveGameRequest): Promise<IJoinLeaveGameResponse> => {
+  console.log("joinGameRequest", joinGameRequest);
   const response: IJoinLeaveGameResponse = {
     joinLeaveResult: JOIN_LEAVE_RESULT.ok
   }
--- backend/actions/leaveGame.ts
+++ backend/actions/leaveGame.ts
@@ -1,6 +1,7 @@
 import { IJoinLeaveGameRequest, IJoinLeaveGameResponse, JOIN_LEAVE_RESULT } from "../../frontend/src/interfaces/IGameList";
 
 export const leaveGame = async (leaveGameRequest: IJoinLeaveGameRequest): Promise<IJoinLeaveGameResponse> => {
+  console.log("leaveGameRequest", leaveGameRequest);
   const response: IJoinLeaveGameResponse = {
     joinLeaveResult: JOIN_LEAVE_RESULT.ok
   }
