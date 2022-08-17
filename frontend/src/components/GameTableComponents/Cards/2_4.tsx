594e4508c0b32c10cae22f90ed32cd2703de4997
Juha Korhonen <juha.korhonen@vincit.fi>
2022-08-17 13:53:52 +0300

    small fixes to card layout

--- frontend/src/components/GameTableComponents/Cards/2_4.tsx
+++ frontend/src/components/GameTableComponents/Cards/2_4.tsx
@@ -9,10 +9,10 @@ interface IProps {
 
 export const xxx = ({title, text, suite}: IProps) => {
   return (
-    <svg width="100%" height="100%" viewBox="0 0 227 315" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+    <svg className="svgBody" width="100%" height="100%" viewBox="0 0 227 315" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
       <title>{title}</title>
-      <text className={`noHover ${suite}`} transform="translate(3.000000, 46.000000)" fontSize={35}>{text}</text>
-      <text className={`noHover ${suite}`} transform="translate(221.000000, 268.000000)" fontSize={35} rotate={180}>{text}</text>
+      <text className={`noHover ${suite}`} transform="translate(5.000000, 46.000000)" fontSize={30}>{text}</text>
+      <text className={`noHover ${suite}`} transform="translate(221.000000, 268.000000)" fontSize={30} rotate={180}>{text}</text>
       <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
         <g id="Clubs" transform="translate(10.000000, 23.000000)" fill="#006400">
           <g id="4" transform="translate(0.000000, 11.000000)" >
