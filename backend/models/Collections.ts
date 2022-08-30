566d0f60fc55293c531721fe1d0bec74e1551e95
Juha Korhonen <juha.korhonen@vincit.fi>
2022-08-30 15:16:12 +0300

    use different collection names for dev

--- backend/models/Collections.ts
+++ backend/models/Collections.ts
@@ -1,5 +1,14 @@
+// these are used in production by old version
+/*
 export const PROMISEWEB_COLLECTION = "promiseweb";
 export const STATS_COLLECTION = "promisewebStats";
 export const PING_COLLECTION = "pingCollection";
 export const USER_COLLECTION = "userCollection";
 export const OBSERVE_COLLECTION = "observeCollection";
+*/
+
+export const PROMISEWEB_COLLECTION = "devPromiseweb";
+export const STATS_COLLECTION = "devPromisewebStats";
+export const PING_COLLECTION = "devPingCollection";
+export const USER_COLLECTION = "devUserCollection";
+export const OBSERVE_COLLECTION = "devObserveCollection";
