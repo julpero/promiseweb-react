afc339c274ec2903e92267994743a161bd733f49
Juha Korhonen <julpero@gmail.com>
2022-09-12 14:54:35 +0300

    cumulative points graph

--- frontend/src/common/commonFunctions.ts
+++ frontend/src/common/commonFunctions.ts
@@ -57,3 +57,22 @@ export const colorize = (str: string): string => {
   const color = Math.floor(Math.abs((Math.sin(hash) * 10000) % 1 * 16777216)).toString(16);
   return "#" + Array(6 - color.length + 1).join("0") + color;
 };
+
+export const increase_brightness = (hex: string, percent: number): string => {
+  // strip the leading # if it's there
+  hex = hex.replace(/^\s*#|\s*$/g, "");
+
+  // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
+  if(hex.length === 3){
+    hex = hex.replace(/(.)/g, "$1$1");
+  }
+
+  const r = parseInt(hex.substring(0, 2), 16);
+  const g = parseInt(hex.substring(2, 4), 16);
+  const b = parseInt(hex.substring(4, 6), 16);
+
+  return "#" +
+     ((0|(1<<8) + r + (256 - r) * percent / 100).toString(16)).substring(1) +
+     ((0|(1<<8) + g + (256 - g) * percent / 100).toString(16)).substring(1) +
+     ((0|(1<<8) + b + (256 - b) * percent / 100).toString(16)).substring(1);
+};
