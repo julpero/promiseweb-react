bf90ee474feebda4c1a365fc92532fbf6aa7381f
Juha Korhonen <julpero@gmail.com>
2022-09-14 14:58:30 +0300

    v2 collections

--- backend/models/Collections.ts
+++ backend/models/Collections.ts
@@ -7,8 +7,8 @@ export const USER_COLLECTION = "userCollection";
 export const OBSERVE_COLLECTION = "observeCollection";
 */
 
-export const PROMISEWEB_COLLECTION = "devPromiseweb";
-export const STATS_COLLECTION = "devPromisewebStats";
-export const PING_COLLECTION = "devPingCollection";
-export const USER_COLLECTION = "devUserCollection";
-export const OBSERVE_COLLECTION = "devObserveCollection";
+export const PROMISEWEB_COLLECTION = "v2Promiseweb";
+export const STATS_COLLECTION = "v2PromisewebStats";
+export const PING_COLLECTION = "v2PingCollection";
+export const USER_COLLECTION = "v2UserCollection";
+export const OBSERVE_COLLECTION = "v2ObserveCollection";
