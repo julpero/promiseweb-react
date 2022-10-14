80d63f3d4db6374463d00c628e22ef58c442a74d
Juha Korhonen <juha.korhonen@vincit.fi>
2022-10-14 10:00:26 +0300

    more methods to tests

--- frontend/tests/commons/methods.ts
+++ frontend/tests/commons/methods.ts
@@ -18,3 +18,51 @@ export const testCheckLoginSuccess = async (page: Page) => {
   await expect(logOutButton).toBeVisible();
   await expect(logOutButton).toBeEnabled();
 };
+
+// const openOpenGamesList = async (page: Page, creatorUser: ITUser) => {
+//   try {
+//     const createGameAccordionButton = page.locator("button", {hasText: /Open Games/});
+//     await createGameAccordionButton.click();
+
+//     try {
+//       await expect(page.locator("li", {hasText: creatorUser.name})).toHaveCount(1);
+//     } catch {
+//       // maybe we just closed the accordion?
+//       // try again
+//       await createGameAccordionButton.click();
+//       await expect(page.locator("li", {hasText: creatorUser.name})).toHaveCount(1);
+//     }
+
+//   } catch (e) {
+//     throw e;
+//   }
+// };
+
+export const joinGame = async (page: Page, currentUser: ITUser, creatorUser: ITUser) => {
+  // await openOpenGamesList(page, creatorUser);
+
+  const joinGameButton = page.locator("button", {hasText: `JOIN GAME - created by ${creatorUser.name}`});
+  const leaveGameButton = page.locator("button", {hasText: `LEAVE GAME - created by ${creatorUser.name}`});
+
+  await expect(joinGameButton).toBeVisible();
+  await expect(joinGameButton).toBeEnabled();
+
+  await expect(leaveGameButton).toBeVisible();
+  await expect(leaveGameButton).toBeDisabled();
+
+  await joinGameButton.click();
+
+  try {
+    // possible error -> retry
+    const closeErrorButton = page.locator("button", {hasText: "Close"});
+    await expect(closeErrorButton).toBeVisible();
+    await expect(closeErrorButton).toBeEnabled();
+    closeErrorButton.click();
+    await expect(joinGameButton).toBeVisible();
+    await expect(joinGameButton).toBeEnabled();
+    await joinGameButton.click();
+  } catch (e) {
+    // all ok, do nothing
+  }
+
+};
