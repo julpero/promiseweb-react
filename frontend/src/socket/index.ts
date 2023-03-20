9c8930f17dfdef167e7434cbe2b4225535d610bf
Juha Korhonen <julpero@gmail.com>
2023-03-20 07:51:54 +0200

    fix socket address

--- frontend/src/socket/index.ts
+++ frontend/src/socket/index.ts
@@ -8,7 +8,7 @@ interface Context {
 
 // console.log("import.meta.env", import.meta.env);
 // console.log("window.location.host", window.location.host);
-const socketUrl = import.meta.env.PROD ? "window.location.host" : "http://localhost:5000";
+const socketUrl = import.meta.env.PROD ? window.location.host : "http://localhost:5000";
 console.log("socketUrl", socketUrl);
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(socketUrl, {
