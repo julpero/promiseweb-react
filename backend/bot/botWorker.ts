52b2e2dd4c73a06d34fca34db32296d19ebd00ed
Juha Korhonen <julpero@gmail.com>
2026-02-22 15:26:40 +0200

    bot randomness and chats

--- backend/bot/botWorker.ts
+++ backend/bot/botWorker.ts
@@ -1,24 +1,27 @@
+import { roundToPlayer } from "../actions/playingGame";
 import { IBotTask, IBotCardPlay, IBotCardPlayResponse, IBotPromise, IBotPromiseResponse } from "../interfaces/IBot";
-import { ICard } from "../interfaces/IGameOptions";
+import { IGameOptions } from "../interfaces/IGameOptions";
 
 // This function runs in a separate thread
 const getBotPromise = (botPromise: IBotPromise): IBotPromiseResponse => {
   // Heavy computation/AI logic here
   console.log("Bot is calculating promise with game state:", botPromise.game);
   return {
-    promise: 1,
+    promise: Math.floor(Math.random() * ((botPromise.game as IGameOptions).game.rounds[botPromise.roundInd].cardsInRound + 1)),
     promiseLogic: "Bot logic for making a promise",
-    promiseChatMessage: "Bot made a promise.",
+    promiseChatMessage: "I just made a random promise.",
   } as IBotPromiseResponse;
 };
 
 const getBotCardPlay = (botCardPlay: IBotCardPlay): IBotCardPlayResponse => {
   // Heavy computation/AI logic here
   console.log("Bot is calculating card play with game state:", botCardPlay.game);
+  const myRound = roundToPlayer(botCardPlay.game as IGameOptions, botCardPlay.roundInd, botCardPlay.botName || "unknown_bot");
+  const randomCard = myRound.playableCards.length > 0 ? myRound.myCards[myRound.playableCards[Math.floor(Math.random() * myRound.playableCards.length)]] : null;
   return {
-    card: {} as ICard,
+    card: randomCard,
     cardLogic: "Bot logic for playing a card",
-    cardChatMessage: "Bot played a card.",
+    cardChatMessage: "I just played a random card.",
   } as IBotCardPlayResponse;
 };
 
