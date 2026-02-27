8d7abbb81dbcca073bf8fa4712daf664667849dd
Juha Korhonen <julpero@gmail.com>
2026-02-27 09:51:49 +0200

    trying to fix bot socket connection

--- backend/bot/botPoolManager.ts
+++ backend/bot/botPoolManager.ts
@@ -1,7 +1,7 @@
 import Piscina from "piscina";
 import path from "path";
 import { IBotCardPlay, IBotCardPlayResponse, IBotMakePromiseRequest, IBotPlayCardRequest, IBotPromise, IBotPromiseResponse, IBotTask } from "../interfaces/IBot";
-import io from "socket.io-client";
+import io, { Socket } from "socket.io-client";
 import { roundToPlayer } from "../actions/playingGame";
 import { IGameOptions } from "../interfaces/IGameOptions";
 
@@ -12,18 +12,19 @@ const workerFileName = path.resolve(
   "worker-loader.js"
 );
 
-const socketEndpoint = process.env.SOCKET_SERVER_URL || "http://localhost:5000";
-console.log("Connecting bot worker to socket server at:", socketEndpoint);
-const socket = io(socketEndpoint, {
-  reconnection: true,
-});
-console.log("Bot worker socket connection status:", socket.connected ? "connected" : "not connected");
-
-const simulateThinkingTimePromise = 1500;
-const simulateThinkingTimeCard = 4000;
+// const socketEndpoint = process.env.SOCKET_SERVER_URL || "http://localhost:5000";
+// console.log("Connecting bot worker to socket server at:", socketEndpoint);
+// const socket = io(socketEndpoint, {
+//   reconnection: true,
+// });
+// console.log("Bot worker socket connection status:", socket.connected ? "connected" : "not connected");
 
 export class BotPoolManager {
   private pool: Piscina;
+  private socket: Socket;
+
+  private simulateThinkingTimePromise = 1500;
+  private simulateThinkingTimeCard = 4000;
 
   constructor() {
     this.pool = new Piscina({
@@ -34,6 +35,13 @@ export class BotPoolManager {
       maxThreads: 4,
       env: process.env,
     });
+
+    const socketEndpoint = process.env.SOCKET_SERVER_URL || "http://localhost:5000";
+    console.log("Connecting bot worker to socket server at:", socketEndpoint);
+    this.socket = io(socketEndpoint, {
+      reconnection: true,
+    });
+    console.log("Bot worker socket connection status:", this.socket.connected ? "connected" : "not connected");
   }
 
   public async getBotPromise(botPromise: IBotPromise): Promise<void> {
@@ -44,9 +52,9 @@ export class BotPoolManager {
       const botTask = { task: "promise", botPromise } as IBotTask;
       const result: IBotPromiseResponse = await this.pool.run(botTask);
       console.log("Bot promise result from worker pool:", result);
-      const thisTimeOut = result.success ? simulateThinkingTimePromise / 8 : simulateThinkingTimePromise; // If the bot failed to get a good promise, respond faster with fallback logic
+      const thisTimeOut = result.success ? this.simulateThinkingTimePromise / 8 : this.simulateThinkingTimePromise; // If the bot failed to get a good promise, respond faster with fallback logic
       setTimeout(() => {
-        socket.emit("make bot promise", {
+        this.socket.emit("make bot promise", {
           promise: result.promise,
           gameId: botPromise.gameId,
           roundInd: botPromise.roundInd,
@@ -62,7 +70,7 @@ export class BotPoolManager {
       console.error("Worker Pool Error:", err);
       // Fallback logic if the worker fails
       setTimeout(() => {
-        socket.emit("make bot promise", {
+        this.socket.emit("make bot promise", {
           // random promise between 0 and count of cards in this round for this bot
           promise: Math.floor(Math.random() * ((botPromise.game as IGameOptions).game.rounds[botPromise.roundInd].cardsInRound + 1)),
           gameId: botPromise.gameId,
@@ -73,7 +81,7 @@ export class BotPoolManager {
           promiseLogic: "Fallback logic",
           promiseChatMessage: "Sorry, I had a brain fart and promised a random number!",
         } as IBotMakePromiseRequest);
-      }, simulateThinkingTimePromise); // Simulate thinking time
+      }, this.simulateThinkingTimePromise); // Simulate thinking time
       return;
     }
   }
@@ -87,9 +95,9 @@ export class BotPoolManager {
       const botTask = { task: "play", botCardPlay } as IBotTask;
       const result: IBotCardPlayResponse = await this.pool.run(botTask);
       console.log("Bot card play result from worker pool:", result);
-      const thisTimeOut = result.success ? simulateThinkingTimeCard / 8 : simulateThinkingTimeCard; // If the bot failed to get a good card, respond faster with fallback logic
+      const thisTimeOut = result.success ? this.simulateThinkingTimeCard / 8 : this.simulateThinkingTimeCard; // If the bot failed to get a good card, respond faster with fallback logic
       setTimeout(() => {
-        socket.emit("play bot card", {
+        this.socket.emit("play bot card", {
           gameId: botCardPlay.gameId,
           card: result.card,
           roundInd: botCardPlay.roundInd,
@@ -106,7 +114,7 @@ export class BotPoolManager {
       // Fallback logic if the worker fails
       const randomCard = myRound.playableCards.length > 0 ? myRound.myCards[myRound.playableCards[Math.floor(Math.random() * myRound.playableCards.length)]] : null;
       setTimeout(() => {
-        socket.emit("play bot card", {
+        this.socket.emit("play bot card", {
           gameId: botCardPlay.gameId,
           card: randomCard,
           roundInd: botCardPlay.roundInd,
@@ -116,7 +124,7 @@ export class BotPoolManager {
           cardPlayLogic: "Fallback logic",
           cardPlayChatMessage: "Sorry, I had a brain fart and played a random card!",
         } as IBotPlayCardRequest);
-      }, simulateThinkingTimeCard); // Simulate thinking time
+      }, this.simulateThinkingTimeCard); // Simulate thinking time
       return;
     }
   }
